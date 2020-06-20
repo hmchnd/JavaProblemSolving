fb93a9e70e3bb2d34853caeddca0b3828a2791e8
hmchnd <shaktimagan@gmail.com>
2020-06-20 13:07:51 +0530

    comiit

--- Introduction/src/UserInput/ScanUserInput.java
+++ Introduction/src/UserInput/ScanUserInput.java
@@ -6,13 +6,12 @@ public class ScanUserInput {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc  = new Scanner(System.in);
-		int principal = sc.nextInt();
-		float rate = sc.nextFloat();
-		int time = sc.nextInt();
-		float simpleInterest = principal*rate*time / 100;
-		System.out.println(simpleInterest);
-		
+	
+		 Scanner sc = new Scanner(System.in); int principal = sc.nextInt(); float rate
+		 = sc.nextFloat(); int time = sc.nextInt(); float simpleInterest =
+		 principal*rate*time / 100;
+		 
+	
 
 	}
 
