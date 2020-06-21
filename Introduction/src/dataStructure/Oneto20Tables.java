9e156f09ebaadb3e61c357339e8486f57af52fdd
hmchnd <shaktimagan@gmail.com>
2020-06-21 22:36:20 +0530

    Patterns qustions on java

---
+++ Introduction/src/dataStructure/Oneto20Tables.java
@@ -0,0 +1,23 @@
+package dataStructure;
+
+public class Oneto20Tables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=20;i++) {
+			
+			for(int j=1;j<=10;j++) {
+				
+				System.out.print(i*j+" ");
+			}
+			
+			System.out.println();
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ Introduction/src/dataStructure/Patterns1.java
@@ -0,0 +1,23 @@
+package dataStructure;
+
+public class Patterns1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=0;i<n;i++) {
+			for(int j=1;j<n;j++) {
+				
+				System.out.print("*"+" ");
+				
+			}
+			
+			System.out.println("*");
+			
+		}
+		
+		
+
+	}
+
+}
